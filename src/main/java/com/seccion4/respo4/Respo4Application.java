402291b06ec00d42b017ad05f3e10dc7cd5a27e3
Racoon <fr.reyesh@duocuc.cl>
2025-04-30 14:29:32 -0400

    primer commit

---
+++ src/main/java/com/seccion4/respo4/Respo4Application.java
@@ -0,0 +1,13 @@
+package com.seccion4.respo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Respo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Respo4Application.class, args);
+	}
+
+}
